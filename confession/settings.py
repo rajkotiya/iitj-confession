d8bc472f3fd786c6aefe7ec0e74d061b1176d90b
rajkotiya <69031687+rajkotiya@users.noreply.github.com>
2021-03-17 16:52:05 +0530

    Update settings.py

--- confession/settings.py
+++ confession/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 
 import os
 
@@ -129,5 +128,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
